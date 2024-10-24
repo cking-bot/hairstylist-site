21089c39048f7a4c82526c9fe05edc4b24b19eae
xt3kn1x <el.sarge@gmail.com>
2024-10-24 18:05:24 -0400

    update base template to use new salon app for base. base views to render

--- salon/views.py
+++ salon/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'salon/home.html')
+
+def services(request):
+    return render(request, 'salon/services.html')
+
+def gallery(request):
+    return render(request, 'salon/gallery.html')
+
+def about(request):
+    return render(request, 'salon/about.html')
+
+def contact(request):
+    return render(request, 'salon/contact.html')
+
+def booking(request):
+    return render(request, 'salon/booking.html')
